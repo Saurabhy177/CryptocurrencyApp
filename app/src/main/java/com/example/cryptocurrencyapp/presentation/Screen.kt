52c1ab324b06cc68e20a4e87f277e07e21141d15
Saurabh Yadav <saurabh.yadav@skuad.io>
2021-10-02 17:17:36 +0530

    updated

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/Screen.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/Screen.kt
@@ -1,5 +1,8 @@
 package com.example.cryptocurrencyapp.presentation
 
+/**
+ * Contains the list of all the screens in app
+ * */
 sealed class Screen(val route: String) {
     object CoinListScreen: Screen("coin_list_screen")
     object CoinDetailScreen: Screen("coin_detail_screen")
